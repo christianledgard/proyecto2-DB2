b0702c1229074ac39af6e05573debfcf6b2a3bbd
Christian Ledgard <39962750+christianledgard@users.noreply.github.com>
2020-11-24 13:36:26 -0500

    merge
    Co-Authored-By: Carlos Reátegui <39816970+CarlosReategui@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -9,107 +9,157 @@ nltk.download('punkt')
 nltk.download('stopwords')
 stemmer = SnowballStemmer('spanish')
 
+class Index:
+    total_documents = 0
 
-def pre_procesamiento(texto):
-    """
-    pre procesar text usando la librería nltk
+    def pre_procesamiento(self, texto):
+        """
+        pre procesar text usando la librería nltk
 
-    :param texto: archivo de texto a procesar
-    """
-    # Generar Tokens
-    tokens = nltk.word_tokenize(texto)
+        :param texto: archivo de texto a procesar
+        """
+        # Generar Tokens
+        tokens = nltk.word_tokenize(texto)
 
-    # StopList
-    stop_list = stopwords.words("spanish")
-    adicionales = ["«", "»", ".", ",", ";", "(", ")", ":", "@", "RT", "#", "|", "?", "!", "https", "$", "%", "&", "'", "''", "..", "..."]
-    stop_list += adicionales
+        # StopList
+        stop_list = stopwords.words("spanish")
+        adicionales = ["«", "»", ".", ",", ";", "(", ")", ":", "@", "RT", "#", "|", "?", "!", "https", "$", "%", "&", "'", "''", "..", "..."]
+        stop_list += adicionales
 
-    # Remover de la lista los StopList Words
-    tokens_clean = tokens.copy()
-    for token in tokens:
-        if token in stop_list:
-            tokens_clean.remove(token)
+        # Remover de la lista los StopList Words
+        tokens_clean = tokens.copy()
+        for token in tokens:
+            if token in stop_list:
+                tokens_clean.remove(token)
 
-    # Remplazar la palabra por su raíz (STEMMING)
-    tokens_stremed = list()
-    for token in tokens_clean:
-        tokens_stremed.append(stemmer.stem(token))
+        # Remplazar la palabra por su raíz (STEMMING)
+        tokens_stremed = list()
+        for token in tokens_clean:
+            tokens_stremed.append(stemmer.stem(token))
 
-    return tokens_stremed
+        return tokens_stremed
 
 
-def dic_palabras_con_terminos_mas_frecuentes(lista):
-    """
-    calcular document frequency
+    def dic_palabras_con_terminos_mas_frecuentes(self, lista):
+        """
+        calcular document frequency
 
-    :param lista: lista con terminos
-    """
-    frecuencia_palabras = dict()
-    for lista_archivo in lista:
-        for item in lista_archivo:
-            if item in frecuencia_palabras:
-                frecuencia_palabras[item][0] += 1
+        :param lista: lista con terminos
+        """
+        frecuencia_palabras = dict()
+        for lista_archivo in lista:
+            for item in lista_archivo:
+                if item in frecuencia_palabras:
+                    frecuencia_palabras[item][0] += 1
+                else:
+                    frecuencia_palabras[item] = [1]
+
+        return dict(sorted(frecuencia_palabras.items(), key=itemgetter(1), reverse=True))
+
+
+    def hallar_coincidencia_de_la_palabra_en_todos_los_tweets(self, palabra, id, index_list):
+        if (len(index_list[palabra]) == 1): index_list[palabra].append([])
+        encontrado = False
+        for elemento_de_lista in index_list[palabra][1]:
+            if elemento_de_lista[0] == id:
+                elemento_de_lista[1] += 1
+                encontrado = True
+        if not encontrado:
+            index_list[palabra][1].append([id,1])
+
+
+    def generar_index(self, grupos_de_palabras_con_id, index_list):
+        for id in grupos_de_palabras_con_id:
+            for palabra in grupos_de_palabras_con_id[id]:
+                self.hallar_coincidencia_de_la_palabra_en_todos_los_tweets(palabra, id, index_list)
+        return dict(sorted(index_list.items()))
+
+
+    def exportar_index(self, index, file_name):
+        with open(file_name, 'w') as file:
+            file.write(json.dumps(index))
+        return True
+
+    def procesar_index(self, nombre_archivo):
+        grupos_de_palabras_con_id = dict()
+        grupos_de_palabras = list()
+        with open(nombre_archivo) as arrayOfJsons:
+            data = json.load(arrayOfJsons)
+            self.total_documents += len(data)
+            for item in data:
+                pre_procesado = self.pre_procesamiento(item["text"])
+                grupos_de_palabras_con_id[item["id"]] = pre_procesado
+                grupos_de_palabras.append(pre_procesado)
+        return self.generar_index(grupos_de_palabras_con_id, self.dic_palabras_con_terminos_mas_frecuentes(grupos_de_palabras))
+
+
+    def bsb_index_construction(self, input_directory, output_directory):
+        for block in os.listdir(input_directory):
+            index = self.procesar_index(input_directory + "/" + block)
+            self.exportar_index(index, output_directory + "/index-" + block)
+            break
+
+
+    def merge_blocks_helper(self, block1, block2):
+        pass # comment explaining why this function is empty
+
+
+
+    def merge_blocks(self, directory):
+        pass #total_merge_blocks = len(os.listdir(directory)) // 2
+
+    def get_file_attributes_for_merge(self, file_name):
+        file = open(file_name)
+        file_dict = json.load(file)
+        file_keys = list(file_dict.keys())
+        file_size = len(file_keys)
+        file.close()
+        return file_dict, file_keys, file_size
+
+    def merge_blocks_2(self, file1, file2, file_destination):
+        # "2019": [3, [[1026857085907226626, 1], [1027004287715553280, 1], [1027005991429332993, 1]]]
+        file1_dict, file1_keys, file1_size = self.get_file_attributes_for_merge(file1)
+        file2_dict, file2_keys, file2_size = self.get_file_attributes_for_merge(file2)
+        i = j = 0
+        merged_dict = dict()
+        while i < file1_size and j < file2_size:
+            word_file1 = file1_keys[i]
+            word_file2 = file2_keys[j]
+            if word_file1 < word_file2:
+                merged_dict[word_file1] = file1_dict[word_file1]
+                i += 1
+            elif word_file1 > word_file2:
+                merged_dict[word_file2] = file2_dict[word_file2]
+                j += 1
             else:
-                frecuencia_palabras[item] = [1]
+                word1_list = file1_dict[file1_keys[i]]
+                word2_list = file2_dict[file2_keys[j]]
+                merged_words = list()
+                merged_words.append(word1_list[0] + word2_list[0])
+                merged_words.append(word1_list[1] + word2_list[1])
+                merged_dict[word_file1] = merged_words
+                i, j = i + 1, j + 1
 
-    return dict(sorted(frecuencia_palabras.items(), key=itemgetter(1), reverse=True))
+        while i < file1_size:
+            merged_dict[file1_keys[i]] = file1_dict[file1_keys[i]]
+            i += 1
+        
+        while j < file2_size:
+            merged_dict[file2_keys[j]] = file2_dict[file2_keys[j]]
+            j += 1
 
-
-def hallar_coincidencia_de_la_palabra_en_todos_los_tweets(palabra, id, index_list):
-    if (len(index_list[palabra]) == 1): index_list[palabra].append([])
-    encontrado = False
-    for elemento_de_lista in index_list[palabra][1]:
-        if elemento_de_lista[0] == id:
-            elemento_de_lista[1] += 1
-            encontrado = True
-    if not encontrado:
-        index_list[palabra][1].append([id,1])
-
-
-def generar_index(grupos_de_palabras_con_id, index_list):
-    for id in grupos_de_palabras_con_id:
-        for palabra in grupos_de_palabras_con_id[id]:
-            hallar_coincidencia_de_la_palabra_en_todos_los_tweets(palabra, id, index_list)
-    return dict(sorted(index_list.items()))
+        self.exportar_index(merged_dict, file_destination)
 
 
-def exportar_index(index, file_name):
-    with open(file_name, 'w') as file:
-        file.write(json.dumps(index))
-    return True
-
-def procesar_index(nombre_archivo):
-    grupos_de_palabras_con_id = dict()
-    grupos_de_palabras = list()
-    with open(nombre_archivo) as arrayOfJsons:
-        data = json.load(arrayOfJsons)
-        for item in data:
-            pre_procesado = pre_procesamiento(item["text"])
-            grupos_de_palabras_con_id[item["id"]] = pre_procesado
-            grupos_de_palabras.append(pre_procesado)
-    return generar_index(grupos_de_palabras_con_id, dic_palabras_con_terminos_mas_frecuentes(grupos_de_palabras))
+    def get_total_documents(self):
+        return self.total_documents
 
 
-def bsb_index_construction(input_directory, output_directory):
-    for block in os.listdir(input_directory):
-        index = procesar_index(input_directory + "/" + block)
-        exportar_index(index, output_directory + "/index-" + block)
+a = Index()
 
-
-def merge_blocks_helper(block1, block2):
-
-
-def merge_blocks(directory):
-    total_merge_blocks = len(os.listdir(directory)) // 2
-
-
-
-# archivo = "clean/tweets_2018-08-07.json"
-# index = procesar_index(archivo)
-# exportar_index(index)
-# print(index)
-
-bsb_index_construction("clean", "index")
+#a.bsb_index_construction("clean", "index")
+#print(a.get_total_documents())
+a.merge_blocks_2("index/index-tweets_2018-08-07.json","index/index-tweets_2018-08-08.json","index/mergedTest.json")
 
 # hallar document frequency (# de documentos que contienen a t). trivial, tamaño de índice invertido sobre un término
 # 
